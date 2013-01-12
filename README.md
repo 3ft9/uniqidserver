65e334bc42c0f7a8a72091e193884e2531b69d67
Stuart Dallas <github@3ft9.com>
2013-01-12 23:04:50 +0000

    Initial implementation.

--- README.md
+++ README.md
@@ -1,4 +1,49 @@
 uniqidserver
 ============
 
-A unique ID server.
+A unique ID server. Runs a TCP server. Connect to that server and it will send you an ID and immediately close the connection.
+
+Usage: uniqidserver <port> <machineid>
+
+The machine ID should be a number between 0 and 255. The number is used when generating IDs. This enables you to run multiple ID servers and ensures that IDs created on different machines will never clash.
+
+IDs are as follows:
+
+    aabbccccccddd
+
+Where:
+
+    aaaa = first four chars of the secs since epoch in hex
+    bb   = the machine ID in hex
+    cccc = the rest of the secs since epoch in hex
+    ddd  = the ms part of time time since epoch in hex
+
+This gives us IDs that are roughly ordered by time, even across servers, that don't have a 00 at the end of every ID if only running one ID server (always looked odd to me), and is horizontally scalable across up to 256 servers. To add more servers simply increase the machine ID from one to two bytes.
+
+If the machine ID is not provided, is -1, or fails to convert to an integer between 0 and 255, the bb part above will be excluded.
+
+License
+-------
+
+No license; no warranty; no nothing. I haven't deployed this to any great capacity yet, so feel free to use it, abuse it, ignore it, or print it out and eat it. Just don't come running to me when it explodes or you throw it all up again :)
+
+Contributing
+------------
+
+Contributions are welcome. Simply fork it, change it, and send a pull request.
+
+TODO
+----
+
+* Add a mode so it can correctly handle HTTP requests so things like PHP's file_get_contents can be used to easily access it.
+
+* Add a mode such that a client connects and then sends a command to get a new ID. This would be for long-running scripts that need to generate a lot of IDs where keeping a connection open would be more efficient than creating one for each ID needed.
+
+Who be me, and why be this?
+---------------------------
+
+Me be Stuart Dallas, and my company is 3ft9 Ltd. I wrote this daemon to solve the problem of creating unique IDs across multiple servers for use with NoSQL databases. I'm aware that there are other established projects that also solve this problem, but I decided to write my own.
+
+Stuart Dallas
+3ft9 Ltd
+http://3ft9.com/
---
+++ uniqidserver.c
@@ -0,0 +1,102 @@
+/**
+ * Unique ID server. Runs a TCP server. Connect to that server and it will send
+ * you an ID and immediately close the connection.
+ *
+ * Usage: uniqidserver <port> <machineid>
+ *
+ * The machine ID should be a number between 0 and 255. The number is used when
+ * generating IDs. This enables you to run multiple ID servers and ensures that
+ * IDs created on different machines will never clash.
+ *
+ * IDs are as follows:
+ *
+ *   aabbccccccddd
+ *
+ * Where:
+ *
+ *   aaaa = first four chars of the secs since epoch in hex
+ *   bb   = the machine ID in hex
+ *   cccc = the rest of the secs since epoch in hex
+ *   ddd  = the ms part of time time since epoch in hex
+ *
+ * This gives us IDs that are roughly ordered by time, even across servers, that
+ * don't have a 00 at the end of every ID if only running one ID server (always
+ * looked odd to me), and is horizontally scalable across up to 256 servers. To
+ * add more servers simply increase the machine ID from one to two bytes.
+ *
+ * If the machine ID is not provided, is -1, or fails to convert to an integer
+ * between 0 and 255, the bb part above will be excluded.
+ */
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+#include <unistd.h>
+
+int main(int argc, char ** argv)
+{
+	int listenfd, connfd, port, machine_id;
+	struct sockaddr_in servaddr, cliaddr;
+	struct timeval tv;
+	socklen_t clilen;
+	char secs_buf[9], id_buf[14];
+	char machine_id_hex[3];
+
+	if (argc != 2 && argc != 3) {
+		printf("Usage: uniqidserver <port> <machineid>\n");
+		return 1;
+	}
+
+	port = atoi(argv[1]);
+	machine_id = (argc == 2 ? -1 : atoi(argv[2]));
+
+	if (machine_id < 0 || machine_id > 255) {
+		fprintf(stderr, "WARN: Not using a machine ID\n");
+		machine_id = -1;
+	} else {
+		// Convert the machine ID to hex.
+		sprintf(machine_id_hex, "%02x", machine_id);
+	}
+
+	while (1) {
+		listenfd = socket(AF_INET, SOCK_STREAM, 0);
+
+		bzero(&servaddr, sizeof(servaddr));
+		servaddr.sin_family = AF_INET;
+		servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+		servaddr.sin_port = htons(port);
+		bind(listenfd, (struct sockaddr *)&servaddr, sizeof(servaddr));
+		listen(listenfd, 1024);
+		clilen = sizeof(cliaddr);
+
+		while (1) {
+			connfd = accept(listenfd, (struct sockaddr *)&cliaddr, &clilen);
+			if (connfd) {
+				if (gettimeofday(&tv, NULL) == -1) {
+					sendto(connfd, "ERR", 3, 0, (struct sockaddr *)&cliaddr, clilen);
+				} else {
+					if (machine_id == -1) {
+						// No machine ID.
+						snprintf(id_buf, 14, "%08x%03x", (unsigned int)tv.tv_sec, tv.tv_usec);
+					} else {
+						// With machine ID.
+						snprintf(secs_buf, 9, "%08x", (unsigned int)tv.tv_sec);
+						snprintf(id_buf, 14, "%c%c%c%c%c%c%c%c%c%c%03x",
+							secs_buf[0], secs_buf[1], secs_buf[2], secs_buf[3],
+							machine_id_hex[0], machine_id_hex[1],
+							secs_buf[4], secs_buf[5], secs_buf[6], secs_buf[7],
+							tv.tv_usec);
+					}
+					sendto(connfd, id_buf, (machine_id == -1 ? 11 : 13), 0, (struct sockaddr *)&cliaddr, clilen);
+				}
+				close(connfd);
+			}
+		}
+
+		close(listenfd);
+	}
+
+	return 0;
+}
